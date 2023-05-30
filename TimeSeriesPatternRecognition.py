18f7caf3fda6a9e4798be19661e3edd75cffecbe
Benjamin Duppe <benjamin.duppe@dfki.de>
2023-05-30 15:46:55 +0200

    New Version for ML-Agent (setup)

--- TimeSeriesPatternRecognition.py
+++ TimeSeriesPatternRecognition.py
@@ -104,7 +104,7 @@ class TimeSeriesPatternRecognition():
 
 
         # Finetuning Labels
-        df_active_phase_all = pd.read_csv(f'{results_root}/evaluation_plausibility_manual_{experiment_number}.csv', header=0, index_col="timestamp", parse_dates=["timestamp"])
+        df_active_phase_all = pd.read_csv(f'{results_root}/evaluation_plausibility_manual_{experiment_number}.csv', header=0, index_col="", parse_dates=[""])
         df_active_phase_plausibility = df_active_phase_all.drop(['ground truth'], axis=1).dropna()
         #df_active_phase_plausibility = df_active_phase_all.fillna(0)
 
