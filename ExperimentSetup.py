f255e676fc0ce07b7c6e24ad888ba2f7409fcdb9
Benjamin Duppe <benjamin.duppe@dfki.de>
2023-06-02 13:00:57 +0200

    New Version for ML-Agent (experiment setup chain)

--- ExperimentSetup.py
+++ ExperimentSetup.py
@@ -34,7 +34,7 @@ def start_task():
     task = Task.init(project_name='NeSy', task_name=f'Experiment Test (Neurosymbolic) {i}')
     # cloned_task = Task.clone(source_task=task)
     # Task.enqueue(task=cloned_task, queue_name='default')
-    task.execute_remotely(queue_name='default', clone=True, exit_process=False)
+    task.execute_remotely(queue_name='default')
 
     os.popen('cp ProblogAddons/bedu.py /root/.clearml/venvs-builds/3.10/lib/python3.10/site-packages/problog/library/bedu.py')
 
