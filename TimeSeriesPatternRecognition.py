1a49c8dd6f5d13c7436748c20e1811d8f48f66ac
Benjamin Duppe <benjamin.duppe@dfki.de>
2023-06-07 12:24:16 +0200

    New Version for ML-Agent (evaluation metrics)

--- TimeSeriesPatternRecognition.py
+++ TimeSeriesPatternRecognition.py
@@ -283,6 +283,7 @@ class TimeSeriesPatternRecognition():
 
         script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
         rel_path = f"{database_root}/Facts/facts_from_ml_sensors_precision_{roman.toRoman(experiment_number)}.json"
+        print(f"Saving Facts from model to '{database_root}/Facts/facts_from_ml_sensors_precision_{roman.toRoman(experiment_number)}.json'")
         abs_file_path = os.path.join(script_dir, rel_path)
 
         with open(abs_file_path, 'w') as facts_file:
